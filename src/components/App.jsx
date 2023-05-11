10b7f66d0fb482783e608e34a6c9293f353a3e6b
Joan <prosandcons500@gmail.com>
2023-05-11 21:31:45 +0300

    change to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { nanoid } from 'nanoid';
@@ -7,20 +7,28 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
 const initState = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+  { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+  { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+  { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
 ];
 
-export class App extends Component {
-  state = {
-    contacts: initState,
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? initState
+  );
+  const [filter, setFilter] = useState('');
 
-  formSubmit = contact => {
-    const { contacts } = this.state;
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+    }
+    localStorage.setItem('contacts', JSON.stringify(contacts) ?? initState);
+  }, [isFirstRender, contacts]);
+
+  const formSubmit = contact => {
     const isExist = contacts.some(
       ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
     );
@@ -37,7 +45,7 @@ export class App extends Component {
       });
       return;
     }
-    this.setState(prevState => ({
+    contacts(prevState => ({
       contacts: [...prevState.contacts, { id: nanoid(), ...contact }],
     }));
     toast.success('Контакт успешно добавлен!', {
@@ -51,56 +59,36 @@ export class App extends Component {
       theme: 'light',
     });
   };
-  clearContact = idContact => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== idContact),
-    }));
+  const clearContact = idContact => {
+    setContacts(contacts.filter(contact => contact.id !== idContact));
   };
-  onChangeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const onChangeFilter = e => {
+    setFilter(e.currentTarget.value);
   };
-  filteredContacts = () => {
-    const { contacts, filter } = this.state;
+  const filteredContacts = () => {
     const normalisedValue = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalisedValue)
     );
   };
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  render() {
-    const { contacts, filter } = this.state;
-    const filteredContacts = this.filteredContacts();
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.formSubmit} />
-        <h2>Contacts</h2>
-        {!!contacts.length ? (
-          <div>
-            <Filter value={filter} onChange={this.onChangeFilter} />
-            <ContactList
-              contacts={filteredContacts}
-              onDeleteContact={this.clearContact}
-            />
-          </div>
-        ) : (
-          <p>There is no contacts here</p>
-        )}
-        <ToastContainer />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={formSubmit} />
+      <h2>Contacts</h2>
+      {!!contacts.length ? (
+        <div>
+          <Filter value={filter} onChange={onChangeFilter} />
+          <ContactList
+            contacts={filteredContacts()}
+            onDeleteContact={clearContact}
+          />
+        </div>
+      ) : (
+        <p>There is no contacts here</p>
+      )}
+      <ToastContainer />
+    </div>
+  );
+};
